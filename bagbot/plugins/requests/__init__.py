d89e3cf63c3a14a3a3c191c6aa91df46abf34f6a
BurnoutAg7 <burnoutag7@gmail.com>
2021-12-18 11:10:26 +0800

    fix bugs

--- bagbot/plugins/requests/__init__.py
+++ bagbot/plugins/requests/__init__.py
@@ -5,11 +5,11 @@ export = export()
 
 @export
 async def send_get(url: str, **kwargs):
-    async with httpx.AsyncClient(proxies=proxy) as client:
+    async with httpx.AsyncClient() as client:
         return await client.get(url, **kwargs)
 
 
 @export
 async def send_post(url: str, **kwargs):
-    async with httpx.AsyncClient(proxies=proxy) as client:
+    async with httpx.AsyncClient() as client:
         return await client.post(url, **kwargs)
