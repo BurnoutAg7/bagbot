97b92fbbbb9d07193f8ee396745482c42e92fc0d
BurnoutAg7 <burnoutag7@gmail.com>
2021-12-21 23:49:01 +0800

    support yandere setus

---
+++ bagbot/plugins/yandere_setu/__init__.py
@@ -0,0 +1,43 @@
+from nonebot import on_startswith
+from nonebot.typing import T_State
+from nonebot.adapters import Bot, Event
+from nonebot.adapters.cqhttp import MessageSegment
+from bagbot.utils import requests
+import random, time
+
+yandere_api = 'https://yande.re/post.json'
+base_params = {
+    'api_version': 2,
+    'limit': 100
+}
+
+setu = on_startswith('yandere')
+ctime = -1
+
+@setu.handle()
+async def deal_setu(bot: Bot, event: Event, state: T_State):
+    args = str(event.get_message()).split()
+    params = base_params
+    if len(args) > 1:
+        params['tags'] = args[1]
+        for i in range(2, len(args)):
+            params['tags'] += '+' + args[i]
+    try:
+        res = await requests.get(yandere_api, params=params)
+        data = res.json()['posts']
+    except Exception as e:
+        await setu.finish('访问yandere API失败，错误为：'+repr(e))
+    if len(data) == 0:
+        await setu.finish('返回列表为空')
+    li = list()
+    for i in range(0, len(data)):
+        if data[i]['rating'] == 'q' and data[i]['score'] >= 4:
+            li.append(i)
+    if not li:
+        await setu.finish('因为设计上的原因没有找到涩图')
+    i = li[random.randint(0, len(li)-1)]
+    url = data[i]['sample_url']
+    description = 'yandereID: ' + str(data[i]['id']) + '\n'
+    description += 'tags: ' + data[i]['tags'] + '\n'
+    await setu.send(description)
+    await setu.finish(MessageSegment.image(url))
