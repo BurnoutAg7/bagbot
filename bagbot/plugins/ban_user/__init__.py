aad7cbd3f2f9317b93ad6efa9cc8f2ef3bc6e7b7
BurnoutAg7 <burnoutag7@gmail.com>
2022-02-21 16:45:40 +0800

    fixing bugs (3/inf)

--- bagbot/plugins/ban_user/__init__.py
+++ bagbot/plugins/ban_user/__init__.py
@@ -16,10 +16,12 @@ async def deal_ban(event: MessageEvent, bot: Bot):
         ban_len = int(ban_len)
     except Exception:
         await ban.finish('时间不是整数')
+    if ban_len < 0:
+        await ban.finish('我不会膜法呢')
     original = event.reply_to_message
     if original == None:
         await ban.finish('请回复一个消息')
     chat = original.get_session_id()
     user = original.get_user_id()
-    bot.ban_chat_member(chat_id=chat, user_id=user, until_date=time()+ban_len)
+    bot.ban_chat_member(chat_id=chat, user_id=user, until_date=int(time())+ban_len)
     await ban.finish('已举办')
